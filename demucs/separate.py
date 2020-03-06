2634c38a8644683854e9794b4f8b4b846fd9ea87
ogut77 <45689370+ogut77@users.noreply.github.com>
2020-03-06 20:36:02 +0300

    Update separate.py

--- demucs/separate.py
+++ demucs/separate.py
@@ -11,6 +11,17 @@ from pathlib import Path
 
 import requests
 import torch as th
+import torch._utils
+try:
+    torch._utils._rebuild_tensor_v2
+except AttributeError:
+    def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
+        tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
+        tensor.requires_grad = requires_grad
+        tensor._backward_hooks = backward_hooks
+        return tensor
+    torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
+    
 import tqdm
 from scipy.io import wavfile
 
