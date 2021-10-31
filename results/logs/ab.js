f4717a4409a05cc16277508acf7576cc5bab9a29
ogut77 <45689370+ogut77@users.noreply.github.com>
2021-10-31 06:46:02 +0400

    Update ab.js

--- results/logs/ab.js
+++ results/logs/ab.js
@@ -89,6 +89,7 @@ $(function() {
     $bar.slider( {
     value : AUDIO.currentTime,
     slide : function(ev, ui) {
+	 VIDEO1.currentTime = VIDEO1.duration/100*ui.value;   
         AUDIO.currentTime = AUDIO.duration/100*ui.value;
        AUDIO1.currentTime = AUDIO1.duration/100*ui.value;
        AUDIO2.currentTime = AUDIO2.duration/100*ui.value;
@@ -102,7 +103,8 @@ $(function() {
          AUDIO1[AUDIO1.paused?'play':'pause'](),
          AUDIO2[AUDIO2.paused?'play':'pause'](),
          AUDIO3[AUDIO3.paused?'play':'pause'](),
-         AUDIO4[AUDIO4.paused?'play':'pause']();
+         AUDIO4[AUDIO4.paused?'play':'pause']()
+	  VIDEO1[VIDEO1.paused?'play':'pause']();
   
   });
   
